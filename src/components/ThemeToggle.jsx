9f0e35e6b1bc7474a54be5dc2bb6479d4a969c7e
dns2140 <davidsagala2140@gmail.com>
2025-07-29 16:32:09 +0700

    improve: light/dark toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 const ThemeToggle = () => {
   const [theme, setTheme] = useState('light');
@@ -12,8 +13,16 @@ const ThemeToggle = () => {
   };
 
   return (
-    <button onClick={toggleTheme} class='themeToggle'>
-      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+    <button
+      onClick={toggleTheme}
+      class='themeToggle'
+      aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+    >
+      {theme === 'light' ? (
+        <FaMoon className='theme-icon' />
+      ) : (
+        <FaSun className='theme-icon' />
+      )}{' '}
     </button>
   );
 };
