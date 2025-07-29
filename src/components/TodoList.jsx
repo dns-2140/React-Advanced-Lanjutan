a007f3e381102b4a038e1d330b0695f703d61353
dns2140 <davidsagala2140@gmail.com>
2025-07-29 17:52:02 +0700

    add: clipboard svg for empty todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Todo from './Todo';
 import { useDispatch } from 'react-redux';
 import { updateTodo, deleteTodo } from '../redux/asyncReducer';
+import { ImGift } from 'react-icons/im';
 
 const TodoList = ({ todos }) => {
   const dispatch = useDispatch();
@@ -12,6 +13,18 @@ const TodoList = ({ todos }) => {
   const handleDelete = (id) => {
     dispatch(deleteTodo(id));
   };
+
+  if (!todos || todos.length === 0) {
+    return (
+      <div className='clipboard'>
+        <img src='./clipboard.svg' alt='' />
+        <span className='firstText'>Belum ada tugas untuk saat ini</span>
+        <span className='secondText'>
+          Silahkan tambah tugas baru pada form di atas
+        </span>
+      </div>
+    );
+  }
   return (
     <div className='todoList'>
       {todos?.map((todo) => (
